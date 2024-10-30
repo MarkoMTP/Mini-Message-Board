31410e84ffec29edcf976d8c6f68da2779d8f3ed
MarkoMTP <marko.mtp8@gmail.com>
2024-10-30 23:02:30 +0100

    added a details button to open message details

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -3,11 +3,13 @@ const indexRouter = Router();
 
 const messages = [
   {
+    id: 1,
     text: "Hi there!",
     user: "Amando",
     added: new Date(),
   },
   {
+    id: 2,
     text: "Hello World!",
     user: "Charles",
     added: new Date(),
@@ -18,11 +20,28 @@ indexRouter.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
+indexRouter.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === messageId);
+
+  if (message) {
+    res.render("messageDetails", { message: message });
+  } else {
+    res.status(404).send("Message not found");
+  }
+});
+
 indexRouter.post("/new", (req, res) => {
   const messageText = req.body.messageText;
   const userName = req.body.user;
+  const latestId = messages[messages.length - 1].id;
 
-  messages.push({ text: messageText, user: userName, added: new Date() });
+  messages.push({
+    id: latestId + 1,
+    text: messageText,
+    user: userName,
+    added: new Date(),
+  });
   res.redirect("/");
 });
 
