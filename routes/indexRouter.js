4260184edeba39013b479ad0245d638614d014f7
MarkoMTP <marko.mtp8@gmail.com>
2024-10-30 22:24:56 +0100

    added route.post for /new

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,23 +1,29 @@
-const {Router } = require('express')
-const indexRouter = Router()
-
+const { Router } = require("express");
+const indexRouter = Router();
 
 const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date()
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date()
-    }
-  ];
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 
-  indexRouter.get('/' , (req, res) => {
-    res.render('index', {messages: messages})
-  })
+indexRouter.get("/", (req, res) => {
+  res.render("index", { messages: messages });
+});
 
-  module.exports = indexRouter;
-  
+indexRouter.post("/new", (req, res) => {
+  const messageText = req.body.messageText;
+  const userName = req.body.user;
+
+  messages.push({ text: messageText, user: userName, added: new Date() });
+  res.redirect("/");
+});
+
+module.exports = indexRouter;
