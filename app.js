f38f7b7ac0d8221c6fc23a5d5a30ca33379ef7b6
MarkoMTP <marko.mtp8@gmail.com>
2024-10-30 22:41:29 +0100

    resolved merge conflicts

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const app = express();
 app.set("view engine", "ejs");
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+app.use(express.urlencoded({ extended: true }));
+
 app.set("views", path.join(__dirname, "views"));
 app.use("/", indexRouter);
 
-app.use('/', indexRouter)
+app.use("/new", newRouter);
 
-  
-  const PORT = 4000;
-  app.listen(PORT, () => {
-    console.log("My first Express App on port  " + `${PORT}`);
-  });
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log("My first Express App on port  " + `${PORT}`);
+});
